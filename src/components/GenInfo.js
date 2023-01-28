5f9c2ee04723c499f4d161a0f0caeb01abe1fc60
Eddie <eddiegabriel4@gmail.com>
2023-01-27 22:52:03 -0800

    added plant family google link, rly cool for learning more about that plant

--- src/components/GenInfo.js
+++ src/components/GenInfo.js
@@ -10,7 +10,8 @@ function GenInfo({common_name, scientific_name, plant_family}) {
             <h3>{common_name}</h3>
             <div className='secondaryInfo'>
                 <p className='scienceName'>{scientific_name}</p>
-                <p className='family'>The {plant_family} family</p>
+                <p className='family' onClick={() => window.open( 'http://www.google.com/search?q=' + plant_family)}>The {plant_family} family</p>
+                
             </div>
         </div>
     )
